b59f45b36ff879735d75f5272e190a467587debc
lius-new <liushui_new@126.com>
2023-08-12 22:24:39 +0800

    feat(chapter-1): 代码出现错误,第二次的时候出现异常,请输入数字,那么这个就是在执行类型转换出现了问题. 难道不可以转换为i32吗

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("请输入你猜测的数字: ");
 
     // rand::thread_rng() 函数提供实际使用随机数生成q器,位于当前执行线程的本地环境中,并从操作系统中获取seed. 接着调用gen_range方法
     // 1..=100: 范围表达式, start..=end
@@ -22,25 +21,28 @@ fn main() {
     // 4. 总之,下面创建了一个可变的字符串变量
     let mut guess = String::new();
 
-    // io::stdio() 代表终端标准输入句柄的类型
-    // read_line() 从标准输入句获取输入并追加到guess ,& 表示引用,其允许程序中多处访问同一个变量而无需在内存中拷贝变量
-    // read_line() 返回result类型. 这是一个枚举类型,它包含两个实例(Ok,Err).  result实例拥有.expect方法.  当实例为Err的时候expect才会生效,此时程序会崩溃并在标准输出终端打印内容
-    io::stdin().read_line(&mut guess).expect("无法读取输入");
+    loop {
+        println!("请输入你猜测的数字: ");
+
+        // io::stdio() 代表终端标准输入句柄的类型
+        // read_line() 从标准输入句获取输入并追加到guess ,& 表示引用,其允许程序中多处访问同一个变量而无需在内存中拷贝变量
+        // read_line() 返回result类型. 这是一个枚举类型,它包含两个实例(Ok,Err).  result实例拥有.expect方法.  当实例为Err的时候expect才会生效,此时程序会崩溃并在标准输出终端打印内容
+        io::stdin().read_line(&mut guess).expect("无法读取输入");
 
 
-    // 将输入中读取的String转换为数字类型
-    // 在rust中通过创建一个新得变量来**隐藏**之前的值(这也是类型转换的常用手法)
-    // parse() 返回的还是一个result类型,包含了OK和Err,并且通过expect来使结果为Err的时候,终止程序并显示指定的错误内容
-    let guess: i32 = guess.trim().parse().expect("请输入数字");
+        // 将输入中读取的String转换为数字类型
+        // 在rust中通过创建一个新得变量来**隐藏**之前的值(这也是类型转换的常用手法)
+        // parse() 返回的还是一个result类型,包含了OK和Err,并且通过expect来使结果为Err的时候,终止程序并显示指定的错误内容
+        let guess: i32 = guess.trim().parse().expect("请输入数字");
 
-    // cmp 用于在任意可比较的变量上调用
-    // match 表示分支
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Equal => println!("win!"),
-        Ordering::Greater => println!("Too big")
+        println!("你输入了: {}", guess);
+
+        // cmp 用于在任意可比较的变量上调用
+        // match 表示分支
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => println!("win!"),
+            Ordering::Greater => println!("Too big")
+        }
     }
-
-
-    println!("你输入了: {}", guess);
 }
