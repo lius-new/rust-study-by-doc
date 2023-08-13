77369d50ca5896197da5d9fbff79d8b7e127d465
lius-new <liushui_new@126.com>
2023-08-13 23:08:56 +0800

    feat(chapter-2/控制流): for

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 fn main() {
-    // while 循环表达式
-    let mut t = 5;
-    while t > 1 {
-        println!("{}", t);
-        t -= 1;
+    // 使用while遍历
+    let arr = [10; 5];
+    let mut index = 0;
+    while index < 5 {
+        println!("{}", arr[index]);
+        index += 1
     }
 
-    let mut counter = 1;
+    // 使用for
+    for item in arr {
+        println!("{}", item);
+    }
 
-    // while 表达式返回的总是(),单元值(单元元组), 因为其没有返回值, 在表达式为false就退出了
-    let result = while counter < 20 {
-        counter += 1;
-        counter
-    };
+    // 使用1..=3来生成一组数据,并进行遍历
+    for item in 1..=3 {
+        println!("{item}")
+    }
+    // 使用1..=3来生成一组数据,然后反转,并进行遍历
+    for item in (1..=3).rev() {
+        println!("{item}")
+    }
+
+    // TODO: RangeInclusive<i32> 类型 ,这是一种什么类型?
+    let item = (1..=3);
 }
