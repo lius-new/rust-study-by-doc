1f459138593f42df419b7204288419a455518523
lius-new <liushui_new@126.com>
2023-08-13 22:25:12 +0800

    feat(chapter-2/数据类型): 函数

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+// 函数命名使用小写,通过_进行单词的分割
+fn main() {
+    println!("Hello,world");
+    another_function()
+}
+
+// rust 不关心函数定义的前后,而关心的是函数所处的作用域
+fn another_function() {
+    println!("Another function.")
+}
+
+// 参数, 注意rust必须为参数指定数据类型
+fn another_function_v2(x: i32) {
+    println!("Another function: {}", x);
+}
