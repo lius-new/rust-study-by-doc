d4b93402e371b382def7dd4d35689c5a9d1efa55
lius-new <liushui_new@126.com>
2023-08-17 17:08:13 +0800

    feat(chapter-5): match 模式匹配的简单使用

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,21 @@
-fn main() {
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
+
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("hello world");
+            1
+        }
+        Coin::Nickel => 2,
+        Coin::Dime => 3,
+        Coin::Quarter => 4,
+    }
+}
+
+fn main() {}
