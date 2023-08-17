3bdc8a9f83053b080f90ab431d423dd99077589e
lius-new <liushui_new@126.com>
2023-08-17 17:19:00 +0800

    feat(chapter-5): match匹配必须要包含所有的可能性

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
     }
 }
 
+// 该段代码的差别在于match不匹配_(None), 此时的match没有匹配到所有的可能(match 是exhaustive(穷尽)),因此无法通过编译.
+fn plus_two(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(x) => Some(x + 1),
+    }
+}
+
 fn main() {
     let five: Option<i32> = Some(5);
 
