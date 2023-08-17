f93be9d2d7311d26ba5cc5e9e0a31d77fdfaae6d
lius-new <liushui_new@126.com>
2023-08-17 14:43:27 +0800

    feat(chapter-5): option类型

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,10 @@
-#[derive(Debug)]
-enum Message {
-    Quit,
-    // 未关联任何数据类型
-    Move { x: i32, y: i32 },
-    // 类似结构体的命名字段
-    Write(String),
-    //包含String
-    ChangeColor(i32, i32, i32), // 包含3个i32
-}
-
-// 为枚举定义对不同实例的同样的处理方法
-impl Message {
-    fn call(&self) {
-        dbg!(&self);
-    }
-}
-// 也可以单独定义四个类型,正如下面这样,但是却不可以向远足那样定义一个供四种类型调用的方法
-
-struct Quit; // 类单元结构体
-
-struct Move {
-    x: i32,
-    y: i32,
-}
-
-struct Write(String); // 元组结构体
-
-struct ChangeColor(i32, i32, i32); // 元组结构体
-
-
 fn main() {
-    Message::Quit.call();
-    Message::Move { x: 4, y: 5 }.call();
-    Message::Write(String::from("hello world")).call();
-    Message::ChangeColor(1, 2, 3).call();
+    // Option 类型, 其表示要么有值(Some),要么无值(None).
+    // Option是一个枚举类型,包含了Some 和 None
+    // Option是十分有用的枚举,其被包含在prelude中,使用它甚至不用显示引入
+
+    let some_number = Some(5);
+    let some_chat = Some('c');
+    let some_nonoe: Option<i32> = None;
+    // 当如果类型是Some那么就意味这是一个非空值,当如果是None那么就是一个空值
 }
