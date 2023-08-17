fb8972a83c91245509856df4299f728a7ec5a8c3
lius-new <liushui_new@126.com>
2023-08-17 22:40:17 +0800

    feat(chapter-6): 在lib(库crate)中定义模块

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_wait_list() {}
+
+        fn seat_at_tablet() {}
+    }
+
+    mod saving {
+        fn take_order() {}
+
+        fn server_order() {}
+
+        fn take_payment() {}
+    }
+}
