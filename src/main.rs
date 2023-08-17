379dafeff3981bff75128e631dc430dae4121ba5
lius-new <liushui_new@126.com>
2023-08-17 14:19:59 +0800

    feat(chapter-5): 枚举的实例创建也可以传递多个参数

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 enum IpAddr {
-    V4(&'static str),
-    V6(&'static str),
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let v4 = IpAddr::V4("127.0.0.1");
-    let v6 = IpAddr::V6("::1");
+    let v4 = IpAddr::V4(127, 0, 0, 1);
+    let v6 = IpAddr::V6(String::from("::1"));
 }
