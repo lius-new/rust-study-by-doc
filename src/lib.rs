9f21d6583ac84c273cb483163cec6f3797198daa
lius-new <liushui_new@126.com>
2023-08-17 23:11:09 +0800

    feat(chpater-6): 移动mod到main或者lib那么被移动mod的子mod也会跟着移动

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // lib crate中声明和二进制crate中声明同样的,好像也不冲突,因为如果lib要使用二进制crate的mod好像不能直接使用,而是要自己也声明这个mod
 // 通过在文件夹中创建mod.rs文件,感觉要比在外面添加mod name.rs文件要好得多. 比如外面如果有一个garden.rs,那么mod一多就会有很多这样的文件了
 
+
 pub mod front_of_house {
     pub mod hosting {
         pub fn add_to_wait_list() {}
