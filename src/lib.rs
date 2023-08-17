16c253441dc9c9b8dc740169a0212ed67f0b4df6
lius-new <liushui_new@126.com>
2023-08-17 23:14:12 +0800

    feat(chapter-6): 不使用super的情况,那么就要从父模块中引入

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,15 @@
 // lib crate中声明和二进制crate中声明同样的,好像也不冲突,因为如果lib要使用二进制crate的mod好像不能直接使用,而是要自己也声明这个mod
 // 通过在文件夹中创建mod.rs文件,感觉要比在外面添加mod name.rs文件要好得多. 比如外面如果有一个garden.rs,那么mod一多就会有很多这样的文件了
+pub mod garden;
 
+fn deliver_order() {}
 
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_wait_list() {}
-
-        fn seat_at_tablet() {}
+mod back_of_house {
+    use crate::deliver_order;
+    fn fix_incorrect_order() {
+        cook_order();
+        deliver_order();
     }
 
-    mod saving {
-        fn take_order() {}
-
-        fn server_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-// 这里好像就是使用crate::来从根引入.
-// 而二进制crate好像是就使用crate名::来从根引入
-pub fn eat_at_restaurant() {
-    // 绝对路径
-    crate::front_of_house::hosting::add_to_wait_list();
-
-    // 相对路径
-    front_of_house::hosting::add_to_wait_list();
+    fn cook_order() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 // 在二进制的根crate中使用mod来创建
-pub mod garden;
+// 事实上移动garden到lib crate,那么发现front_of_house也移动到lib crate中,也就是说mod在哪那么其内部的crate就以谁为父节点
 
 // 绝对路径: 以crate开头的全路径.对于外部crate代码以crate名开头的绝对路径. 对于当前crate的代码,则以字面值crate开头
 // 还是使用crate名::mod名这样的形式使用lib中的mod,因为使用crate::lib::modname报错了
-use rust_32::front_of_house;  // 事实上好像main 是二进制crate的根而lib是库crate的根,他们表示的crate并不是同一个意思.那么如果要使用lib crate,那么指定crate::lib::... , 感觉还不如直接crate名::mod名.
+// use rust_32::front_of_house;  // 事实上好像main 是二进制crate的根而lib是库crate的根,他们表示的crate并不是同一个意思.那么如果要使用lib crate,那么指定crate::lib::... , 感觉还不如直接crate名::mod名.
+
+use rust_32::garden::{*};
 
 fn main() {
     let asparagus = garden::vegetables::Asparagus {};
     dbg!(asparagus);
 
+    vegetables::front_of_house::hosting::add_to_wait_list();
     front_of_house::hosting::add_to_wait_list();
 }
