56521290f6051e1dff6a4def9b1f5f56073fe236
lius-new <liushui_new@126.com>
2023-08-17 17:15:47 +0800

    feat(chapter-5): 使用match来匹配Option类型

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("hello world");
-            1
-        }
-        Coin::Nickel => 2,
-        Coin::Dime => 3,
-        Coin::Quarter(usState) => {
-            println!("State quarter from {:?}!", state);
-            4
-        }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(x) => Some(x + 1),
+        _ => None
     }
 }
 
-fn main() {}
+fn main() {
+    let five: Option<i32> = Some(5);
+
+    let five_plus = plus_one(five);
+
+    let res = plus_one(None);
+}
