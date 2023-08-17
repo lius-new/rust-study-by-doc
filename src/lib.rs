e5b7137ad2281aa2cb12c66f3fbc4fd3ceb38f1f
lius-new <liushui_new@126.com>
2023-08-17 23:30:38 +0800

    feat(chapter-6): 使用pub将use进入的内容再重新导出作用域

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // 可以通过as来设置被use的新得名称
-use std::io::Result as IoResult;
+pub use std::io::Result as IoResult;
 
 // lib crate中声明和二进制crate中声明同样的,好像也不冲突,因为如果lib要使用二进制crate的mod好像不能直接使用,而是要自己也声明这个mod
 // 通过在文件夹中创建mod.rs文件,感觉要比在外面添加mod name.rs文件要好得多. 比如外面如果有一个garden.rs,那么mod一多就会有很多这样的文件了
