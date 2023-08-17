709c7601ee3a614ce65e74fd6a4e012b00993ba7
lius-new <liushui_new@126.com>
2023-08-17 14:18:15 +0800

    feat(chapter-5): 使用枚举的每个实例添加类型来替换前面枚举和结构体一起用的方式

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-// 定义一个枚举的类型
-#[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    // 枚举的成员
-    V6,
+enum IpAddr {
+    V4(&'static str),
+    V6(&'static str),
 }
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: &'static str,
-}
-
 
 fn main() {
-    let ipaddr1 = IpAddr {
-        kind: IpAddrKind::V4,
-        address: "localhost",
-    };
-    let ipaddr2 = IpAddr {
-        kind: IpAddrKind::V6,
-        address: "::127.0.0.1",
-    };
+    let v4 = IpAddr::V4("127.0.0.1");
+    let v6 = IpAddr::V6("::1");
 }
