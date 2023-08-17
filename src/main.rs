8df02fda008d1847bccd63d74b742872095ce3c5
lius-new <liushui_new@126.com>
2023-08-17 14:32:39 +0800

    feat(chapter-5): 和枚举相似的类型,可却有很多结构体,并且结构体没有上面所谓的公共的方法

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,19 @@ impl Message {
         dbg!(&self);
     }
 }
+// 也可以单独定义四个类型,正如下面这样,但是却不可以向远足那样定义一个供四种类型调用的方法
+
+struct Quit; // 类单元结构体
+
+struct Move {
+    x: i32,
+    y: i32,
+}
+
+struct Write(String); // 元组结构体
+
+struct ChangeColor(i32, i32, i32); // 元组结构体
+
 
 fn main() {
     Message::Quit.call();
