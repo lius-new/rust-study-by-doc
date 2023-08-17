3ea3ac8e1f5609bf77260ebe89b7615775f2f40b
lius-new <liushui_new@126.com>
2023-08-17 14:31:44 +0800

    feat(chapter-5): 一个包含各种类型的枚举,并且定义了供枚举中各个类型实例统一的方法

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-struct IpAddV4 {}
-
-struct IpAddV6 {}
-
-enum IpAddr {
-    V4(IpAddV4),
-    V6(IpAddV6),
+#[derive(Debug)]
+enum Message {
+    Quit,
+    // 未关联任何数据类型
+    Move { x: i32, y: i32 },
+    // 类似结构体的命名字段
+    Write(String),
+    //包含String
+    ChangeColor(i32, i32, i32), // 包含3个i32
 }
 
-fn main() {}
+// 为枚举定义对不同实例的同样的处理方法
+impl Message {
+    fn call(&self) {
+        dbg!(&self);
+    }
+}
+
+fn main() {
+    Message::Quit.call();
+    Message::Move { x: 4, y: 5 }.call();
+    Message::Write(String::from("hello world")).call();
+    Message::ChangeColor(1, 2, 3).call();
+}
