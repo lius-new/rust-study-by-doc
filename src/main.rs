486d31a4ef7f11cb5a518e67ecc407b6070b020c
lius-new <liushui_new@126.com>
2023-08-17 10:50:01 +0800

    feat(chapter-4): 结构化示例程序,使用元组而非单个值

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,19 @@ fn main() {
     let width = 30;
     let height = 50;
 
-    println!("The area of the rectangle is {} square pixels.", area(width, height))
+    println!("The area of the rectangle is {} square pixels.", area(width, height));
+    println!("The area of the rectangle is {} square pixels.", area_v2((height, width)));
+    println!("The area of the rectangle is {} square pixels.", area_v3((height, width)));
 }
 
 fn area(width: i32, height: i32) -> i32 {
     width * height
+}
+
+fn area_v2((width, height): (i32, i32)) -> i32 {
+    width * height
+}
+
+fn area_v3(dimensions: (i32, i32)) -> i32 {
+    dimensions.0 * dimensions.1
 }
