e570ddd702e85045abffc0095abb7d001d6f4576
lius-new <liushui_new@126.com>
2023-08-17 23:20:27 +0800

    feat(chapter-6): 使用pub来标记其为公有,结构体和枚举也是这样的

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,30 @@ pub mod garden;
 fn deliver_order() {}
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("world"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
     }
 
     fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("bye");
+    meal.toast;
+    meal.seasonal_fruit; // 此时无法访问,因为其并不是pub
 }
