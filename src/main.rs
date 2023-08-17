47a18d97d7ef0ec5e20e7fa2021731b6d0544594
lius-new <liushui_new@126.com>
2023-08-17 17:26:26 +0800

    feat(chapter-5): match必须要匹配所有可能的值

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,6 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
     }
 }
 
-// 该段代码的差别在于match不匹配_(None), 此时的match没有匹配到所有的可能(match 是exhaustive(穷尽)),因此无法通过编译.
-fn plus_two(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(x) => Some(x + 1),
-    }
-}
 
 fn main() {
     let five: Option<i32> = Some(5);
@@ -18,4 +12,15 @@ fn main() {
     let five_plus = plus_one(five);
 
     let res = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => 3,
+        7 => 7,
+        o => o,
+    };
+    // 注意使用other来匹配所有特殊列出来的值. 这样就满足了match必须穷尽的值.
+    // 注意匹配的时候分支的顺序,因为match是通过顺寻来匹配的.
+
+    // rust 提供_模式,当我们不想要使用通配模式获取的值时,可以使用_,可以匹配任意值,但是不绑定到该值上.(不绑定表示忽略匹配的值),而后在_对应的代码块中执行操作.
 }
