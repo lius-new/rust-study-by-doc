4a8a6cce39818c98fe1959e430c23298d62a29eb
lius-new <liushui_new@126.com>
2023-08-18 22:07:50 +0800

    feat(chatper-7): string字符串的简单使用

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-
+    // 字符串就是作为字节的集合外加一些方法实现的.
+    // 事实上String被实现为一个带有一些额外保证限制功能的字节vector的封装
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
 }
