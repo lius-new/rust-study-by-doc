4dacc6c263053c09a90e65441a7f1e928c9c8c3e
lius-new <liushui_new@126.com>
2023-08-18 22:22:17 +0800

    feat(chapter-7): 字符串的索引和切片

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,18 @@ fn main() {
     s.push_str("hello");
 
     // 创建两个字符串
-    let s1 = String::from("Hello, ");
+    let s1 = String::from("你Hello, ");
     let s2 = String::from("world!");
 
     // + 的函数签名,第一个参数会丢失所有权,第二个参数需要是一个引用
     // fn add(self, s: &str) -> String {
     let s3 = s1 + &s2;
-    println!("{}", s1); // s1 的所有权已经转移到s3
+    // println!("{}", s1); // s1 的所有权已经转移到s3
 
     // 使用format来拼接字符串
-    format!("{s1} - {s2}");
+    // format!("{s1} - {s2}");
+
+    // 字符串不支持索引
+    // 字符串的切片, 这也是很容易出现问题,因为往往字节的长度不是想象的那样,比如0..1,那么在为字母的时候没有问题,但是如果是中文,那么就该是0..3,因为其占据三个字符长度
+    println!("{}", &s3[0..1]);
 }
