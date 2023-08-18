67d578ecb3842322558d4212e0c560b3d5226913
lius-new <liushui_new@126.com>
2023-08-18 22:28:58 +0800

    feat(chapter-7): hashMap简单使用

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,11 @@
+use std::collections::{HashMap};
+
 fn main() {
+
+    // HashMap的使用.
+    // HashMap将数据存储在堆上. HashMap的数据类型是同质的. 这表示key和value的类型可以是不一样的,但是其各自的类型必须是一样的
+    let mut hash_map = HashMap::new();
+    hash_map.insert(String::from("hello"), 0);
+    hash_map.insert(String::from("world"), 0);
+
 }
