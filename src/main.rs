4bea946c8f74b34b2aa0a043e8de0f4a71852a21
lius-new <liushui_new@126.com>
2023-08-18 22:59:36 +0800

    feat(chapter-9): 当有多段代码逻辑相似的时候

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,23 @@
 fn main() {
+    // 泛型, 被设计用来处理重复的逻辑的工具
+
+    let number_list = vec![1, 2, 10, 3];
+    let mut largest = &number_list[0];
+
+    for number in number_list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![203, 89, 10, 8392];
+    let mut largest = &number_list[0];
+
+    for number in number_list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("The largest number is {}", largest);
 }
