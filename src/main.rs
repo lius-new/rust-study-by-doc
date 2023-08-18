4d9b451261bb3c0385b7204ee626f933858d0ce8
lius-new <liushui_new@126.com>
2023-08-18 23:02:34 +0800

    feat(chapter-9): 通过函数类封装重复的代码

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
+fn lagrest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for number in list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
 fn main() {
     // 泛型, 被设计用来处理重复的逻辑的工具
 
     let number_list = vec![1, 2, 10, 3];
-    let mut largest = &number_list[0];
-
-    for number in number_list.iter() {
-        if number > largest {
-            largest = number;
-        }
-    }
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", lagrest(&number_list));
 
     let number_list = vec![203, 89, 10, 8392];
-    let mut largest = &number_list[0];
-
-    for number in number_list.iter() {
-        if number > largest {
-            largest = number;
-        }
-    }
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", lagrest(&number_list));
 }
