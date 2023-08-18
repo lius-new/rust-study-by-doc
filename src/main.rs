4c23a8a78de0759f59d6bd5c0bb1df9bd76a974c
lius-new <liushui_new@126.com>
2023-08-18 22:32:21 +0800

    feat(chapter-7): hashMap值的获取

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,10 @@ fn main() {
     hash_map.insert(String::from("hello"), 0);
     hash_map.insert(String::from("world"), 0);
 
+    // get方法获取的是Option<&i32>类型,通过copied来获取Option<i32>类型,接着调用unwrap_or且指定没有值的时候使用0
+    let r = hash_map.get("world").copied().unwrap_or(0);
+
+    for (key, value) in hash_map.iter() {
+        println!("{key}:{value}")
+    }
 }
