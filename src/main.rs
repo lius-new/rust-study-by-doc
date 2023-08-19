ad77cf211932e18f9acbd1a2bd143548b51c4b9b
lius-new <liushui_new@126.com>
2023-08-19 21:39:02 +0800

    feat(chapter-9): 通过Bound的写法来地体在函数中指定trait参数的写法

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,16 @@ pub fn notify(item: &impl Summary) {
     println!("breaking news:{}", item.summarize())
 }
 
+// trait bound 语法(写法), 这样的写法和之前的写法是一样的。
+pub fn notify_v2<T: Summary>(item: &T) {
+    println!("breaking news:{}", item.summarize())
+}
+
+// 如果有多个泛型的参数呢?
+pub fn notify_v3<T: Summary>(param1: &T, param2: &T) {
+    println!("breaking news:{} / {}", param1.summarize(), param2.summarize())
+}
+
 fn main() {
     // trait
     // 类型的行为是由类型的方法决定的。如果不同的类型调用相同的方法，那么这些类型就可以共享相同的方法了。
