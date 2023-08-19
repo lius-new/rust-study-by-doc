fe7c452eb4032d8f8ce9f6eab59f808329a0888d
lius-new <liushui_new@126.com>
2023-08-19 21:23:23 +0800

    feat(chapter-9): 为各种类型实现trait

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
+use std::fmt::{Display, Formatter};
+
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("hello world")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        todo!()
+    }
+}
+
+impl Display for NewsArticle {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Summary for dyn Display {
+    fn summarize(&self) -> String {
+        todo!()
+    }
+}
+
+
+impl Summary for Vec<T> {
+    fn summarize(&self) -> String {
+        todo!()
+    }
 }
 
 fn main() {
